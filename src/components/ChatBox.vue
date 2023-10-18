80f673fdbd18a2a3bd7c0f9b33172c6d46e43f78
sinnedpenguin <sinnedpenguin@proton.me>
2023-10-18 21:58:29 +0800

    Adjust font size for code blocks

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -95,7 +95,7 @@ function handleClearChat() {
 
 function formatMessage(message: string) {
   let formattedMessage = message
-    .replace(/```(.*?)```/gs, '<div class="relative bg-primary text-white dark:text-black rounded-md"><pre style="font-size: 0.875rem;"><code>$1</code></pre><button class="absolute top-0 right-0 m-1 copy-button">Copy</button></div>')
+    .replace(/```(.*?)```/gs, '<div class="relative bg-primary text-white dark:text-black rounded-md"><pre style="font-size: 0.825rem;"><code>$1</code></pre><button class="absolute top-0 right-0 m-1 copy-button">Copy</button></div>')
     .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
     .replace(/\n/g, '<br />'); 
 
