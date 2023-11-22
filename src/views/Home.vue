a51b97448f7066e588f18bb71c785b47d38d6db8
sinnedpenguin <sinnedpenguin@proton.me>
2023-11-23 01:37:13 +0800

    Add Discord alert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import ChatBox from '@/components/ChatBox.vue';
+import DiscordAlert from '@/components/DiscordAlert.vue';
 </script>
 
 <template>
   <section class="flex justify-center mt-6">
     <div class="md:w-[35vw] mb-2">
+      <DiscordAlert />
       <ChatBox />
     </div>
   </section>
